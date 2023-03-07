8d7234261c31b708c8337669156ab17119329dd4
emrecoban <fbdarktole@gmail.com>
2023-03-07 21:06:13 +0300

    Resolved

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,12 @@ import React from "react";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import Home from './components/Home';
 
-import { HelmetProvider } from 'react-helmet-async';
-
 export default function App(){
-    
-    const helmetContext = {};
     return (
-        <HelmetProvider context={helmetContext}>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/:kelime?" element={<Home/>} />
-                </Routes>
-            </BrowserRouter>
-        </HelmetProvider>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/:kelime?" element={<Home/>} />
+            </Routes>
+        </BrowserRouter>
     )
 }
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,11 +2,6 @@ import React from "react";
 import Output from "./Output";
 import Autowords from '../autocomplete';
 
-import * as htmlToImage from 'html-to-image';
-import { Helmet } from 'react-helmet-async';
-
-
-
 export default function Form({darkMode, urlTakip}){
     const [kelime, setKelime] = React.useState({show:false, kelime:""})
     const [anlam, setAnlam] = React.useState([])
@@ -14,7 +9,6 @@ export default function Form({darkMode, urlTakip}){
     const [loader, setLoader] = React.useState(urlTakip ? true : false)
     const [kelimeOner, setKelimeOner] = React.useState("")
     const [oneriKutusu, setOneriKutusu] = React.useState(true)
-    const [metaImg, setMetaImg] = React.useState(null)
 
     async function fetchTDK(word){
         const res = await fetch("https://sozluk.gov.tr/gts?ara=" + word)
@@ -67,26 +61,6 @@ export default function Form({darkMode, urlTakip}){
         setOneriKutusu(true)
     }, [kelimeOner])
 
-    function createImg(){
-        htmlToImage.toPng(document.getElementById('pageToImg'))
-                    .then(function (dataUrl) {
-                        const img = new Image();
-                        img.src = dataUrl;
-                        //document.body.appendChild(img);
-                        setMetaImg(dataUrl)
-                        console.log("resim geldi: ", metaImg)
-                    })
-                    .catch(function (error) {
-                        console.error('oops, something went wrong! htmlToImage: ', error);
-                    });
-    }
-
-    React.useEffect(()=>{
-        createImg()
-    }, [outputs])
-
-    
-
     return (
         <>
             <form onSubmit={handleForm}>
@@ -104,32 +78,15 @@ export default function Form({darkMode, urlTakip}){
             {
                 kelimeOner.length > 2 && Autowords.filter(oneri=>oneri.madde.startsWith(kelimeOner)).slice(0,7).map((oneriP, i)=><p className={oneriKutusu ? darkMode ? "onerilenler dark" : "onerilenler" : "onerilenler hide"} key={i}><b>{kelimeOner}</b>{oneriP.madde.split(kelimeOner)[1]}</p>)
             }
-            <div id="pageToImg">
-                <h1 className="outputWord">{kelime.show && kelime.kelime}</h1>
-                {
-                    loader ? 
-                    <>
-                        <br /><div className="loader"></div><br />
-                    </>
-                    : 
-                    <>
-                        {outputs}
-                        <Helmet>
-                            <title>{kelime.kelime} Anlamı - Türkçe Sözlük</title>
-                            <meta name="description" content={`${kelime.kelime} Anlamı - Türkçe Sözlük`} />
-                            <meta property="og:title" content={`${kelime.kelime} - Türkçe Sözlük`} />
-                            <meta property="og:description" content={`${kelime.kelime} Anlamı - Türkçe Sözlük`} />
-                            <meta property="twitter:title" content={`${kelime.kelime} - Türkçe Sözlük`} />
-                            <meta property="twitter:description" content={`${kelime.kelime} Anlamı - Türkçe Sözlük`} />
-                            <meta name="twitter:creator" content="@emreshepherd" />
-                            <meta name="twitter:card" content="summary_large_image" />
-                            <meta name="twitter:image" content="https://i.imgur.com/H3vOUS6.png" />
-                            <meta name="og:image" content="https://i.imgur.com/H3vOUS6.png" />
-                        </Helmet>
-                    </>
-                    
-                }
-            </div>
+            <h1 className="outputWord">{kelime.show && kelime.kelime}</h1>
+            {
+                loader ? 
+                <>
+                    <br /><div className="loader"></div><br />
+                </>
+                : outputs
+                
+            }
         </>
     )
 }
