43944bc9d75271313ba85af3615b595abdc27bad
emrecoban <fbdarktole@gmail.com>
2023-04-28 19:05:02 +0300

    Added text to logo.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 
-export default function Header({toggleDarkMode, darkMode}){
+export default function Header({ toggleDarkMode, darkMode }) {
     return (
         <nav>
-            <i className="fa-solid fa-book logo"></i>
-            <div className="darkMode-Wrapper">
-                <input type="checkbox" id="darkMode" className="sc-gJwTLC ikxBAC" onChange={toggleDarkMode} checked={darkMode} />
+            <div className={darkMode ? "mainLogo dark" : "mainLogo"}>
+                <i className="fa-solid fa-book logo"></i>
+                <p>Sözlük</p>
             </div>
-            <label htmlFor="darkMode">
-                <i className="fa-regular fa-moon darkModeLogo"></i>
-            </label>
+            <div className="rightPanel">
+                <div className="darkMode-Wrapper">
+                    <input type="checkbox" id="darkMode" className="sc-gJwTLC ikxBAC" onChange={toggleDarkMode} checked={darkMode} />
+                </div>
+                <label htmlFor="darkMode">
+                    <i className="fa-regular fa-moon darkModeLogo"></i>
+                </label>
+            </div>
         </nav>
     )
 }
